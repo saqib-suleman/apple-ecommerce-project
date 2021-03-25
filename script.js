1f89592d2c3896f7f03d8422addd022a20fdb187
Saqib Suleman <saqibs3291@gmail.com>
2021-03-26 01:22:17 +0500

    add section 4 javascript

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.querySelectorAll(".watch-control, .controls a").forEach((control) => {
+  control.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
+
 //Cube
 let x = 0;
 let y = 20;
@@ -107,3 +113,62 @@ window.addEventListener("scroll", () => {
   }
 });
 // End of Section 3
+
+// Section 4
+const watchBands = document.querySelector(".watch-bands");
+const watchCases = document.querySelector(".watch-cases");
+
+const watchTopControl = document.querySelector(".watch-top-control");
+const watchRightControl = document.querySelector(".watch-right-control");
+const watchBottomControl = document.querySelector(".watch-bottom-control");
+const watchLeftControl = document.querySelector(".watch-left-control");
+
+let axisX = 0;
+let axisY = 0;
+
+const hideControl = () => {
+  if (axisY === -280) {
+    watchTopControl.classList.add("hide-control");
+  } else {
+    watchTopControl.classList.remove("hide-control");
+  }
+
+  if (axisY === 280) {
+    watchBottomControl.classList.add("hide-control");
+  } else {
+    watchBottomControl.classList.remove("hide-control");
+  }
+
+  if (axisX === 280) {
+    watchRightControl.classList.add("hide-control");
+  } else {
+    watchRightControl.classList.remove("hide-control");
+  }
+
+  if (axisX === -280) {
+    watchLeftControl.classList.add("hide-control");
+  } else {
+    watchLeftControl.classList.remove("hide-control");
+  }
+};
+
+watchTopControl.addEventListener("click", () => {
+  watchCases.style.marginTop = `${(axisY -= 70)}rem`;
+  hideControl();
+});
+
+watchBottomControl.addEventListener("click", () => {
+  watchCases.style.marginTop = `${(axisY += 70)}rem`;
+  hideControl();
+});
+
+watchRightControl.addEventListener("click", () => {
+  watchBands.style.marginRight = `${(axisX += 70)}rem`;
+  hideControl();
+});
+
+watchLeftControl.addEventListener("click", () => {
+  watchBands.style.marginRight = `${(axisX -= 70)}rem`;
+  hideControl();
+});
+// End of Section 4
