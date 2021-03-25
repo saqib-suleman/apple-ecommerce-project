d9a6096ca922f2893cb951022297afa542d1a4ba
Saqib Suleman <saqibs3291@gmail.com>
2021-03-25 21:42:31 +0500

    add logic for cube controls

--- script.js
+++ script.js
@@ -1,10 +1,62 @@
-// document
-//   .querySelector("meta[name=viewport]")
-//   .setAttribute(
-//     "content",
-//     "width=device-width, initial-scale=" + 1 / window.devicePixelRatio
-//   );
+//Cube
+let x = 0;
+let y = 0;
+let z = 0;
+let bool = true;
+let interval;
 
+const cube = document.querySelector(".cube");
+
+document.querySelector(".top-x-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${(x += 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".bottom-x-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${(x -= 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".left-y-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y -= 20)}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".right-y-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y += 20)}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".top-z-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z -= 20)}deg)`;
+});
+
+document.querySelector(".bottom-z-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z += 20)}deg)`;
+});
+
+document.querySelector(".reset").addEventListener("click", () => {
+  cube.style.transform = `rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
+});
+
+const playPause = () => {
+  if (bool) {
+    interval = setInterval(() => {
+      cube.style.transform = `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`;
+    }, 100);
+  } else {
+    clearInterval(interval);
+  }
+};
+playPause();
+
+document.querySelector(".controls").addEventListener("mouseover", () => {
+  bool = false;
+  playPause();
+});
+document.querySelector(".controls").addEventListener("mouseout", () => {
+  bool = true;
+  playPause();
+});
+//End of Cube
+
+//SlideShow
 const slideshowDivs = (() => {
   for (let i = 1; i <= 5; i++) {
     const div = document.createElement("div");
@@ -38,3 +90,4 @@ const slideshow = (() => {
     }
   }, 20000);
 })();
+// End of SlideShow
